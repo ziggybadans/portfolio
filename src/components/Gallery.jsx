9d82fcdb4677d6a3ff12a84e0d8f4c5651b39024
ziggybadans <ziggybadans@hotmail.com.au>
2023-04-10 19:42:52 +1000

    Removed lightbox animations for now, fixed lightbox

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,72 +1,75 @@
 import styles from "../components/Gallery.module.scss";
-import Image from "../components/Image.jsx"
-
+import React, { useState } from "react";
+import { motion } from 'framer-motion';
+import data from "../data/images.json"
+import Lightbox from "./Lightbox";
 
 function Gallery() {
+
+    const [ openImg, setOpenImg ] = useState(null);
+    const [ currentIndex, setCurrentIndex ] = useState(null);
+    var scrollingEnabled;
+
+    const handleClick = (item, index) => {
+        setCurrentIndex(index);
+        setOpenImg(item.link);
+        if (screenSize > 1000) {disableScrolling()} else {enableScrolling()};
+    };
+
+    const handleRotationRight = () => {
+        const totalLength = data.data.length;
+        if(currentIndex + 1 >= totalLength) {
+            setCurrentIndex(0);
+            const newUrl = data.data[0].link;
+            setOpenImg(newUrl);
+            return;
+        }
+        else {
+            const newIndex = currentIndex + 1;
+            const newUrl = data.data.filter((item) => {
+                return data.data.indexOf(item) === newIndex;
+            });
+            const newItem = newUrl[0].link;
+            setOpenImg(newItem);
+            setCurrentIndex(newIndex);
+        }
+    }
+
+    if (typeof window !== "undefined") {
+        var screenSize = window.innerWidth;
+      }
+
+    function disableScrolling() {
+        var x = window.scrollX;
+        var y = window.scrollY;
+        window.onscroll = function() {
+            window.scrollTo(x, y);
+        };
+        scrollingEnabled = false;
+    }
+
+    function enableScrolling() {
+        window.onscroll = function () {}
+        scrollingEnabled = true;
+    }
+
     return (
-            <div className={styles.gallery} style={{zIndex: '0'}}>
-                <Image style={styles.griditem} src="images/image1.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image2.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image48.jpg" alt="" />
-                <Image style={styles.griditemportrait} src="images/image54.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image8.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image27.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image11.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image7.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image9.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image5.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image10.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image6.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image12.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image13.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image14.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image15.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image16.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image17.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image18.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image19.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image20.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image21.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image22.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image23.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image24.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image25.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image26.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image3.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image28.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image29.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image30.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image31.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image32.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image33.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image34.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image35.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image36.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image37.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image38.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image39.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image40.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image41.jpg" alt="" />
-                <Image style={styles.griditemlandscape} src="images/image42.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image43.jpg" alt="" />
-                <Image style={styles.griditembig} src="images/image44.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image45.jpg" alt="" />
-                <Image style={styles.griditemportrait} src="images/image46.jpg" alt="" />
-                <Image style={styles.griditemportrait} src="images/image47.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image4.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image49.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image50.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image51.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image52.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image53.jpg" alt="" />
-                <Image style={styles.griditemportrait} src="images/image55.jpg" alt="" />
-                <Image style={styles.griditemlandscape} src="images/image56.jpg" alt="" />
-                <Image style={styles.griditem} src="images/image60.jpg" alt="" />
-                <Image style={styles.griditemlandscape} src="images/image58.jpg" alt="" />
-                <Image style={styles.griditemportrait} src="images/image59.jpg" alt="" />
-                <Image style={styles.griditemportrait} src="images/image61.jpg" alt="" />
+        <div>
+            <div className={styles.gallery}>
+                {data.data.map((item, index) => (
+                    <motion.div key={index} id={styles[item.size]}
+                        initial={{opacity:0}}
+                        whileInView={{opacity:1}}
+                        whileHover={{scale:1.1, boxShadow: "0px 0px 8px rgba(0,0,0,0.3)"}}
+                        whileTap={{scale:1.05}}
+                        style={{cursor:"pointer"}}>
+                            <img className={styles.griditem} id={styles[item.size]} src={item.link} alt={item.text} onClick={() => handleClick(item, index)}/>
+                    </motion.div>
+                ))}
+            {openImg && <Lightbox openImg={openImg} handleRotationRight={handleRotationRight} setOpenImg={setOpenImg}/>}
             </div>
-    )
+        </div>
+    );
 }
 
 export default Gallery;
--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import styles from "../components/Lightbox.module.scss";
 import { m, motion } from 'framer-motion';
+import React, { useState } from "react";
 
-const Lightbox = ({children, style, src, alt, onClick}) => {
-    const [ isOpen, setIsOpen ] = useState(false);
-    const [ isOpenSmall, setIsOpenSmall ] = useState(false);
+const Lightbox = ({ openImg, handleRotationRight, setOpenImg }) => {
 
-    function scroll() {
-        var scrollDiv = document.getElementById("agent-div").offsetTop;
-        window.scrollTo({ top: scrollDiv, behavior: 'smooth'})
-    }
+    const [ zoom, enableZoom ] = useState(false);
 
-    function disableScrolling() {
-        var x = window.scrollX;
-        var y = window.scrollY;
-        window.onscroll = function() {
-            window.scrollTo(x, y);
-        };
-    }
+    const handleClick = (e) => {
+        if(e.target.classList.contains("dismiss")) {
+            setOpenImg(null);
+            enableScrolling();
+        } else {
+            if (screenSize > 500) { enableZoom(!zoom) }
+            console.log(zoom);
+        }
+    };
 
     function enableScrolling() {
         window.onscroll = function () {}
@@ -24,44 +22,16 @@ const Lightbox = ({children, style, src, alt, onClick}) => {
 
     if (typeof window !== "undefined") {
         var screenSize = window.innerWidth;
+        console.log(screenSize);
       }
 
-
-    const toggleIsOpen = () => {
-        if (screenSize < 1000) {setIsOpenSmall(!isOpenSmall)};
-        if (screenSize > 1000) {setIsOpen(!isOpen)};
-        if (isOpen == false && screenSize > 1000) {scroll()};
-        if (isOpen == false && screenSize > 1000) {disableScrolling()} else {enableScrolling()};
-    };
-
     return (
-        <motion.div Layout id="agent-div" className={style} onClick={toggleIsOpen}
-            animate={{
-                scale: isOpen ? 4.5 : 1,
-                x: isOpen ? "160%" : "0%",
-                y: isOpen ? "50%" : "0%",
-                boxShadow: "0px 0px 8px rgba(0,0,0,0.3)",
-                zIndex: isOpen ? "10" : "0"
-            }}>
-            {children}
-            {isOpen &&
-                <motion.div className="lightbox-container" onClick={toggleIsOpen} style={{
-                    position: 'absolute',
-                    display: 'flex',
-                    top: '0',
-                    left: '0',
-                    height: '100vh',
-                    width: '100vw',
-                    background: 'rgba(255,255,255,0.5)',
-                    cursor: 'pointer',
-                    zIndex: '1'
-                }}
-                animate={{
-                    x: isOpen ? -100 : 0,
-                    y: isOpen ? -100 : 0
-                }}>
-                </motion.div>
-            }
+        <motion.div className={styles.overlay} onClick={handleClick}>
+            <motion.img src={openImg}
+                animate={{ scale: zoom ? 2 : 1 }}
+                transition={{ ease: "easeOut", duration: 0.0005}}
+                style={{ cursor: zoom ? "zoom-out" : "zoom-in" }}/>
+            <span className="dismiss" onClick={handleClick} style={{cursor:"pointer"}}>X</span>
         </motion.div>
     );
 };
