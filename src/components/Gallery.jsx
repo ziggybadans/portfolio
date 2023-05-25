572747cff2877564f895b3c29545a0dc80996ac0
ziggybadans <ziggybadans@hotmail.com.au>
2023-05-25 13:14:48 +1000

    Added filter system

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,6 +10,9 @@ import Lightbox from "./Lightbox";
 
 function Gallery({ jsonFile }) {
   const [images, setImages] = useState([]);
+  const [filter, setFilter] = useState("all")
+  const filters = ["all", "nature", "wildlife", "architecture", "sky", "portrait", "landscape", "bands", "abstract", "cars"]
+
   const [openImg, setOpenImg] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(null);
 
@@ -24,6 +27,18 @@ function Gallery({ jsonFile }) {
       .then(data => setImages(data.data))
       .catch(error => console.error(error));
   }, [jsonFile]);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const getFilteredImages = () => {
+    if (filter === "all") {
+      return images;
+    } else {
+      return images.filter((image) => image.tags.includes(filter));
+    }
+  };
 
   const handleClick = useCallback(
     (item, index) => {
@@ -73,8 +88,29 @@ function Gallery({ jsonFile }) {
 
   return (
     <div>
+        <div className="flex justify-center items-center mb-10">
+        <div className="relative inline-flex">
+          <select className="bg-white dark:bg-black appearance-none rounded-full p-2 pl-3 pr-10" value={filter} onChange={handleFilterChange}>
+            {filters.map((filter) => (
+              <option key={filter} value={filter}>
+                {filter.charAt(0).toUpperCase() + filter.slice(1)}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4 transform rotate-180"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M7.05 11.293l3.591-3.909a.697.697 0 0 1 1.041 0l3.909 3.909a.802.802 0 0 1 0 1.114.776.776 0 0 1-1.114 0l-3.272-3.576-3.27 3.576a.776.776 0 0 1-1.114 0 .802.802 0 0 1 0-1.114z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
       <div className={styles.gallery}>
-        {images.map((item, index) => (
+        {getFilteredImages().map((item, index) => (
           <motion.div
             key={index}
             id={styles[item.size]}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,10 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class',
+  variants: {
+    extend: {
+      backgroundColor: ['dark']
+    }
+  }
 }
