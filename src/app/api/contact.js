fd8fd6c1a5a8429da4b8200d894b170cd2b76414
ziggybadans <ziggybadans@hotmail.com.au>
2023-11-18 00:46:28 +1100

    Redid contact.js actually

--- src/app/api/contact.js
+++ src/app/api/contact.js
@@ -1,49 +1,38 @@
 import sgMail from "@sendgrid/mail"
-import multer from "multer"
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const upload = multer;
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
+sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const form = upload.array("fileUpload")(req, res, async function (err) {
-            if (err instanceof multer.MulterError) {
-                res.status(500).json({ error: err.message });
-            } else if (err) {
-                res.status(500).json({ error: err.message });
-            } else {
-                const { email, name } = req.body;
-                const attachments = req.files.map((file) => ({
-                    filename: file.originalname,
-                    content: file.buffer.toString('base64'),
-                    type: file.mimetype,
-                    disposition: 'attachment',
-                }));
+    const { firstName, lastName, email, subject, message, region, phone } = req.body;
 
-                const msg = {
-                    to: 'ziggybadans@gmail.com',
-                    from: email,
-                    subject: `New contact from ${name}`,
-                    attachments,
-                };
+    if (firstName === '' || email === '' || subject === '' || message === '') {
+        return res.status(400).json({ error: 'The first name, email, subject and message fields are required. Please fill in those fields and try again.' });
+    }
 
-                try {
-                    await sgMail.send(msg);
-                    res.status(200).json({ done: true });
-                } catch (error) {
-                    console.error(error);
-                    res.status(500).json({ error: 'An error occurred while sending email' });
-                }
-            }
-        });
-    } else {
-        res.status(500).json({ error: 'Wrong request method' });
+    const content = {
+        to: 'ziggybadans@gmail.com',
+        from: email,
+        subject: `New message from ${firstName} ${lastName}`,
+        text: message,
+        html: `
+            <h1>Details</h1>
+            <hr />
+            <ul>
+            <li><b>Firstname:</b> ${firstName}</li>
+            ${lastName ? `<li><b>Lastname:</b> ${lastName}</li>` : ''}
+            <li><b>Email:</b> ${email}</li>
+            ${region ? `<li><b>Region:</b> ${region}</li>` : ''}
+            ${phone ? `<li><b>Phone:</b> ${phone}</li>` : ''}
+            <li><b>Subject:</b> ${subject}</li>
+            <li><b>Message:</b> ${message}</li>
+        `,
+    };
+
+    try {
+        await sgMail.send(content);
+        return res.status(200).json({ error: '' });
+    } catch (error) {
+        console.log('ERROR ', error);
+        return res.status(400).json({ error: 'Failed to send the message.' });
     }
 }
